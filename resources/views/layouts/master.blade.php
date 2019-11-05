cd86e0e6c76378ff79da1fcb21be3f37be2cf106
3a632081 <s3a632081@student.ncut.edu.tw>
2019-11-05 17:01:21 +0800

    3-d 修改主樣板內<link>標籤當中 css 檔案的參考位置(url)

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -131,6 +131,16 @@
 <!-- Theme JavaScript -->
 <script src="js/clean-blog.min.js"></script>
 
+<!-- Bootstrap Core CSS -->
+<link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
+<!-- Theme CSS -->
+<link href="{{asset('css/clean-blog.min.css')}}"
+      rel="stylesheet">
+<!-- Custom Fonts -->
+<link href="{{asset('css/font-awesome.min.css')}}"
+      rel="stylesheet" type="text/css">
+
+
 </body>
 
 </html>
