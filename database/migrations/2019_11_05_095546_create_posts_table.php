6228aa1bee4e6e357039031e907ec1f20b6819eb
3a632081 <s3a632081@student.ncut.edu.tw>
2019-11-05 17:58:12 +0800

    練習3:在新建立的 migration 檔案裡，撰寫 up方法的內容

--- database/migrations/2019_11_05_095546_create_posts_table.php
+++ database/migrations/2019_11_05_095546_create_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePostsTable extends Migration
+class  CreatePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -16,6 +16,9 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->timestamps();
+            $table->string('title');
+            $table->text('content');
+
         });
     }
 
